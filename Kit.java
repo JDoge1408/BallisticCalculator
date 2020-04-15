c78ba52d8723961b8516755e3943380f9f194dc9
JDoge1408 <jrkriefall408@gmail.com>
2020-04-15 16:42:47 -0500

    Update Kit.java

--- Kit.java
+++ Kit.java
@@ -2,32 +2,37 @@ public class Kit {
 	private double scopeOffset;
 	private int zeroDistance;
 	private String name;
-	//private int caliber;
-	
+	private String caliber;
+
 	public Kit() {
 		scopeOffset = 0;
 		zeroDistance = 0;
 	}
-	
-	public Kit(double s, int z, String n) {
-		setSpecs(s, z, n);
+
+	public Kit(double s, int z, String n, String c) {
+		setSpecs(s, z, n, c);
 	}
-	
-	public void setSpecs(double s, int z, String n) {
+
+	public void setSpecs(double s, int z, String n, String c) {
 		scopeOffset = s;
 		zeroDistance = z;
 		name = n;
+		caliber = c;
 	}
-	
+
 	public double getOffset() {
 		return scopeOffset;
 	}
-	
+
 	public int getZero() {
 		return zeroDistance;
 	}
-	
+
 	public String getName() {
 		return name;
+	}
+
+	public String getCaliber() {
+		return caliber;
 	}
 }
